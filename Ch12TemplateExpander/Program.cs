18a81e03f4ee8e7833bd9be87e0656c2645e3cce
hashjenny <>
2025-12-16 21:12:46 +0800

    add ch12 sketch code and migrate .sln to .slnx

---
+++ Ch12TemplateExpander/Program.cs
@@ -0,0 +1,16 @@
+﻿using Ch12TemplateExpander;
+
+var dom = """
+          <html>
+            <body>
+              <ul z-loop="item:names">
+                <li><span z-var="item"/></li>
+              </ul>
+            </body>
+          </html>
+          """;
+
+Dictionary<string, Data> data = new()
+{
+    ["names"] = new ArrayData(["Johnson", "Vaughan", "Jackson"])
+};
